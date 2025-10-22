a1cfcd15f252e3da22fa542449eafa4221efe1f2
Anusree <anilkumaranusree113@gmail.com>
2025-10-22 19:54:05 +0530

    fix: update file 011-567-permutation-in-string.js

--- dsa-chatgpt/011-567-permutation-in-string.js
+++ dsa-chatgpt/011-567-permutation-in-string.js
@@ -1,32 +1,45 @@
 // 567. permutation-in-string
 
-        arr[s1.charCodeAt(i) - aCode]++
-    }
-    for (let i = 0; i < s2.length; i++) {
-        arr2[s2.charCodeAt(i) - aCode]++
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {
+  // permutation as in same letters and same count, together
+  // letter similarity and count is checked on array
+  // that they are always together is checked by keeping
+  // fixed window length
 
-    const arr2 = new Array(26).fill(0)
+  if (s1.length > s2.length) return false;
 
-        if (i - left + 1 > s1.length) {
-    let left = 0
-            arr2[s2.charCodeAt(left) - aCode]--
-        }
+  const arr = new Array(26).fill(0);
+  const aCode = "a".charCodeAt(0);
+  for (let i = 0; i < s1.length; i++) {
+    arr[s1.charCodeAt(i) - aCode]++;
+  }
 
-            left++
-                if (arr[j] !== arr2[j]) {
-        if (i - left + 1 === s1.length) {
-            for (let j = 0; j < 26; j++) {
-            let match = true
-                    match = false
-                    break
-                }
-            }
-            if (match) return true
-        }
-    }
+  const arr2 = new Array(26).fill(0);
+  let left = 0;
+  for (let i = 0; i < s2.length; i++) {
+    arr2[s2.charCodeAt(i) - aCode]++;
 
-    return false
-    for (let i = 0; i < s1.length; i++) {
-    const aCode = 'a'.charCodeAt(0)
-    const arr = new Array(26).fill(0)
+    if (i - left + 1 > s1.length) {
+      arr2[s2.charCodeAt(left) - aCode]--;
+      left++;
+    }
+
+    if (i - left + 1 === s1.length) {
+      let match = true;
+      for (let j = 0; j < 26; j++) {
+        if (arr[j] !== arr2[j]) {
+          match = false;
+          break;
+        }
+      }
+      if (match) return true;
+    }
+  }
+
+  return false;
 };
