bbe44c018bd70afdf1def7280122b9fa1d2aa20f
Anusree Anilkumar <anilkumaranusree113@gmail.com>
2025-10-23 14:07:48 +0530

    Add LeetCode solution: reverse-linked-list

---
+++ dsa-chatgpt/017-206-reverse-linked-list.js
@@ -0,0 +1,26 @@
+// 206. reverse-linked-list
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+
+    let prev = null
+    let curr = head
+
+    while (curr) {
+        const next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+};
