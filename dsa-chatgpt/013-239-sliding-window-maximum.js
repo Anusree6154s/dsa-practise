82e88540498e182acd182e3ed6c053028e1a31a4
Anusree <anilkumaranusree113@gmail.com>
2025-10-23 11:18:27 +0530

    update 013-239-sliding-window-maximum.js

--- dsa-chatgpt/013-239-sliding-window-maximum.js
+++ dsa-chatgpt/013-239-sliding-window-maximum.js
@@ -1,33 +1,37 @@
 // 239. sliding-window-maximum
 
 
-
-    for (let i = 0; i < nums.length; i++) {
-        if (indexDeque.length && indexDeque[0] <= i - k) {
-            indexDeque.shift()
-        }
-
-        while (indexDeque.length && nums[indexDeque[indexDeque.
-length - 1]] < nums[i]) {
-            indexDeque.pop()
-        }
-
-        indexDeque.push(i)
-
-
-        if (i >= k - 1) {
-            res.push(nums[indexDeque[0]])
-        }
-        // pop all smaller elements
-        // shift first elemt smaller than window
-        // push latest elem
-        // push first elem of deque into result each window 
-        // (it is the max value per window)
-    const indexDeque = []
-    const res = []
-var maxSlidingWindow = function (nums, k) {
- * @return {number[]}
- */
- * @param {number} k
- * @param {number[]} nums
 /**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+    const indexDeque = []
+    const res = []
+
+
+    for (let i = 0; i < nums.length; i++) {
+        // shift first elemt smaller than window
+        if (indexDeque.length && indexDeque[0] <= i - k) {
+            indexDeque.shift()
+        }
+
+        // pop all smaller elements
+        while (indexDeque.length && nums[indexDeque[indexDeque.length - 1]] < nums[i]) {
+            indexDeque.pop()
+        }
+
+        // push latest elem
+        indexDeque.push(i)
+
+
+        // push first elem of deque into result each window 
+        // (it is the max value per window)
+        if (i >= k - 1) {
+            res.push(nums[indexDeque[0]])
+        }
+    }
+
+    return res
+};
