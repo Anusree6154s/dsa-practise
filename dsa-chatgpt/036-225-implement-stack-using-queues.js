6f0e98861f593cfe21d4b8d979caf5906071a070
Anusree Anilkumar <anilkumaranusree113@gmail.com>
2025-10-25 07:44:13 +0530

    Add LeetCode solution: implement-stack-using-queues

---
+++ dsa-chatgpt/036-225-implement-stack-using-queues.js
@@ -0,0 +1,56 @@
+// 225. implement-stack-using-queues
+
+
+var MyStack = function () {
+    this.q1 = []
+    this.q2 = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+    // to always keep newly pusehd elements at top
+
+    this.q2.push(x) // new el in q2 alone
+
+    while (this.q1.length > 0) {
+        this.q2.push(this.q1.shift()) //add rest q1 elements after it
+    }
+
+    //exchange arrays to keep q2 empty
+    let temp = this.q1
+    this.q1 = this.q2
+    this.q2 = temp
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+    return this.q1.shift()
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+    return this.q1[0]
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+    return this.q1.length === 0
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
